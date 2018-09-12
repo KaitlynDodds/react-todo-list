03eb124ee321265013dc6f0af895682e906519dc
KaitlynDodds <kadodds.dev@kadodds.com>
2018-09-12 10:48:30 -0600

    basic babel and webpack setup complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { hot } from "react-hot-loader";
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <h1>Welcome to Todo List</h1>
+                <p>This is the beginning</p>
+                <p>Another change has been made</p>
+            </div>
+        );
+    }
+}
+
+export default hot(module)(App);
